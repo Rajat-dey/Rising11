2ecb65a23e8bb5af67b30c6b7cdd267bc2900e50
Rajat-dey <dey.rajat33@gmail.com>
2019-05-23 02:02:28 +0530

    Signin,splash,signup,welcome screen done

--- app/src/main/java/com/line/rising11/SignIn.java
+++ app/src/main/java/com/line/rising11/SignIn.java
@@ -1,13 +1,50 @@
 package com.line.rising11;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignIn extends AppCompatActivity {
+
+    Button register, signin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        register=findViewById(R.id.Register);
+        signin=findViewById(R.id.btn_login);
+
+
+
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignIn.this, SignUp.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignIn.this, Login.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
+
+
+
+
+
+
 }
