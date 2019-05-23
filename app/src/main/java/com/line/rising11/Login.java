1b7db7db0c6397cc51f179ee72aade267c8ba942
Rajat-dey <dey.rajat33@gmail.com>
2019-05-23 11:13:59 +0530

    create contest added

--- app/src/main/java/com/line/rising11/Login.java
+++ app/src/main/java/com/line/rising11/Login.java
@@ -1,13 +1,33 @@
 package com.line.rising11;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+
+    Button contest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        contest=findViewById(R.id.signup);
+
+
+        contest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, CreateContest.class);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
 }
