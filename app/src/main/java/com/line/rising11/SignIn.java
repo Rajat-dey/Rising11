b3a219ae51e1428b65f89571c394f2ebc0d293c7
Vishnu Sharma <bochiwal.visnu@gmail.com>
2019-05-27 00:06:50 +0530

    >> Add Navigation Drawer UI >> Added Home Screen and its tabs UI

--- app/src/main/java/com/line/rising11/SignIn.java
+++ app/src/main/java/com/line/rising11/SignIn.java
@@ -34,7 +34,7 @@ public class SignIn extends AppCompatActivity {
         signin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(SignIn.this, Login.class);
+                Intent intent = new Intent(SignIn.this, HomeActivity.class);
                 startActivity(intent);
             }
         });
