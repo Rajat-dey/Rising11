5d594d8e5d8fa1716e8b53fe6287754c715dd9ae
vivekagrawal1997 <agrawalvivek1297@gmail.com>
2019-05-30 19:30:07 +0530

    login logout done.

--- app/src/main/java/com/line/rising11/Login.java
+++ app/src/main/java/com/line/rising11/Login.java
@@ -1,6 +1,11 @@
 package com.line.rising11;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -25,7 +30,8 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        SharedPreferences sharedPreferences=getSharedPreferences("loginstatus", Context.MODE_PRIVATE);
+        final SharedPreferences.Editor editor=sharedPreferences.edit();
 
         contest=findViewById(R.id.signup);
         login=findViewById(R.id.btn_login);
@@ -68,50 +74,67 @@ public class Login extends AppCompatActivity {
                 else
                 {
                     //http://rising11.com/apps/apis/login.php
-                    JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
-                            (Request.Method.GET, getString(R.string.signin)+"?mobile="+email.getText().toString().trim()+"&password="+password.getText().toString().trim(), null, new Response.Listener<JSONObject>() {
-                                @Override
-                                public void onResponse(JSONObject response) {
-                                    // Log.d("Response: ", response.toString());
-                                    //Log.d("Link",getString(R.string.signup) +"?mobile="+email.getText().toString().trim()+"&password="+password.getText().toString().trim());
+                    boolean connected = false;
+                    ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+                    if(connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED ||
+                            connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED) {
+
+                        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                                (Request.Method.GET, getString(R.string.signin)+"?mobile="+email.getText().toString().trim()+"&password="+password.getText().toString().trim(), null, new Response.Listener<JSONObject>() {
+                                    @Override
+                                    public void onResponse(JSONObject response) {
+                                        // Log.d("Response: ", response.toString());
+                                        //Log.d("Link",getString(R.string.signup) +"?mobile="+email.getText().toString().trim()+"&password="+password.getText().toString().trim());
 
 
-                                    try {
-                                        if(response.getString("code").equals("1"))
-                                        {
-                                            Intent intent = new Intent(Login.this, HomeActivity.class);
-                                            startActivity(intent);
+                                        try {
+                                            if(response.getString("code").equals("1"))
+                                            {
 
-                                            Toast.makeText(Login.this, response.getString("msg"), Toast.LENGTH_SHORT).show();
-                                        }
-                                        else
-                                        {
-                                            Toast.makeText(Login.this, response.getString("msg"), Toast.LENGTH_SHORT).show();
+                                                editor.putString("login","yes");
+                                                editor.putString("number",email.getText().toString().trim());
+                                                editor.commit();
+                                                Toast.makeText(Login.this, response.getString("msg"), Toast.LENGTH_SHORT).show();
+                                                Intent intent = new Intent(Login.this, HomeActivity.class);
+                                                startActivity(intent);
+                                                finish();
+                                            }
+                                            else
+                                            {
+                                                Toast.makeText(Login.this, response.getString("msg"), Toast.LENGTH_SHORT).show();
                                                /* Toast.makeText(getApplicationContext(), "Error "
                                                         + response.getString("code") + ": "
                                                         + response.getString("message"), Toast.LENGTH_LONG)
                                                         .show();*/
+                                            }
+                                        } catch (JSONException e) {
+                                            e.printStackTrace();
                                         }
-                                    } catch (JSONException e) {
-                                        e.printStackTrace();
+
                                     }
+                                }, new Response.ErrorListener() {
 
-                                }
-                            }, new Response.ErrorListener() {
+                                    @Override
+                                    public void onErrorResponse(VolleyError error) {
+                                        // TODO: Handle error
+                                        Toast.makeText(getApplicationContext(), "Error: "
+                                                + error.getLocalizedMessage(), Toast.LENGTH_LONG)
+                                                .show();
+                                    }
+                                });
 
-                                @Override
-                                public void onErrorResponse(VolleyError error) {
-                                    // TODO: Handle error
-                                    Toast.makeText(getApplicationContext(), "Error: "
-                                            + error.getLocalizedMessage(), Toast.LENGTH_LONG)
-                                            .show();
-                                }
-                            });
+                        // Access the RequestQueue through your singleton class.
+                        RestClient.getInstance(Login.this).addToRequestQueue(jsonObjectRequest);
 
-                    // Access the RequestQueue through your singleton class.
-                    RestClient.getInstance(Login.this).addToRequestQueue(jsonObjectRequest);
-                    Intent intent = new Intent(Login.this, HomeActivity.class);
-                    startActivity(intent);
+
+
+                        connected = true;
+                    }
+                    else
+                    {
+                        Snackbar.make(v, "Please check your Internet connection", Snackbar.LENGTH_LONG)
+                                .setAction("Action", null).show();
+                    }
 
 
                 }
