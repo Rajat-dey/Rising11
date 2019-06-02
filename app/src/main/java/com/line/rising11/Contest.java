e6ee7bc90b4628f5c36011670f0a09aa82561342
Rajat-dey <dey.rajat33@gmail.com>
2019-06-02 14:01:57 +0530

    player_points added and layout created

--- app/src/main/java/com/line/rising11/Contest.java
+++ app/src/main/java/com/line/rising11/Contest.java
@@ -1,14 +1,32 @@
 package com.line.rising11;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Contest extends AppCompatActivity {
 
+    TextView playerpoints;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contest);
         setTitle("JOINED CONTEST");
+
+
+        playerpoints=findViewById(R.id.player_points);
+
+
+        playerpoints.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent=new Intent(Contest.this,player_points.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
