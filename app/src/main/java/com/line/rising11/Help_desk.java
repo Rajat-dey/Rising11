10f140cece9a072405eb969dcbcd9d3e410a37da
Rajat-dey <dey.rajat33@gmail.com>
2019-06-08 01:33:20 +0530

    help desk social media data

--- app/src/main/java/com/line/rising11/Help_desk.java
+++ app/src/main/java/com/line/rising11/Help_desk.java
@@ -1,14 +1,47 @@
 package com.line.rising11;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Help_desk extends AppCompatActivity {
 
+    ImageView fb,insta,twitter,yt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help_desk);
         setTitle("Help Desk");
+
+        fb=findViewById(R.id.fb);
+
+
+
+
+
+
+
+
+        fb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = getIntent();
+                Uri data = intent.getData();
+
+
+               startActivity(intent);
+
+
+             //   Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(data));
+
+            }
+        });
+
+
+
     }
 }
