9cdd490d2c333ef717eaaf04a91edc60187bcc37
Rajat-dey <dey.rajat33@gmail.com>
2019-06-11 22:13:02 +0530

    home activity condition removed

--- app/src/main/java/com/line/rising11/HomeFragment.java
+++ app/src/main/java/com/line/rising11/HomeFragment.java
@@ -107,23 +107,10 @@ public class HomeFragment extends Fragment {
                                     for(int i=0;i<array.length();i++)
                                     {
 
-                                        if(array.getJSONObject(i).get("unique_id").toString().equals("1166934"))
-                                        {
-                                            jsonArray.put(array.getJSONObject(i));
-                                        }
-                                        if(array.getJSONObject(i).get("unique_id").toString().equals("1144495"))
-                                        {
-                                            jsonArray.put(array.getJSONObject(i));
-                                        }
-                                        if(array.getJSONObject(i).get("unique_id").toString().equals("1166935"))
-                                        {
-                                            jsonArray.put(array.getJSONObject(i));
-                                        }
 
-                                        if(array.getJSONObject(i).get("unique_id").toString().equals("1177889"))
-                                        {
                                             jsonArray.put(array.getJSONObject(i));
-                                        }
+
+
 
                                     }
                                     rvHomeMatches.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
